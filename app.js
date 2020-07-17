41a00421d29034c1ef68affaa8da16a126d46bec
Kate Sowles <hello@katesowles.com>
2020-07-16 17:20:12 -0700

    updated core packages and took care of deprecations along the way

--- app.js
+++ app.js
@@ -5,7 +5,11 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , errorHandler = require('errorhandler')
+  , logger = require('morgan')
+  , methodOverride = require('method-override');
 
 var app = express();
 
@@ -13,21 +17,19 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
